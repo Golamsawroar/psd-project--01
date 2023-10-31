5f4ddbf48567d57d2ab1c5050e3113304e0a5f06
golamsawoar <golamsawroar420@gmail.com>
2023-10-31 15:44:00 +0600

    all project add

---
+++ jss/script.js
@@ -0,0 +1,11 @@
+
+/* ===============================js start====================================== */
+$('.main_slaid').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows:true,
+    prevArrow:'<i class="fa-solid fa-chevron-left prev"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-right next"></i>',
+  });
+/* ===============================js end====================================== */
